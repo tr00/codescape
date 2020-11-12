7c00c8f16087ff6369349562ba667dade9b2a790
tr00 <tryyyy@gmx.de>
2020-11-12 12:08:43 +0100

    3 cmds

--- Deck1/50_50_Chance.java
+++ Deck1/50_50_Chance.java
@@ -2,23 +2,22 @@ import codescape.Dogbot;
 public class MyDogbot extends Dogbot {
 
     public void run() {
-		// Stringausgabe: System.out.println(String);
-		// Stringvergleich: String1.equals(String2);
-		// Die Ausgabe von read() in diesem Raum ist entweder "L" oder "R"
-
-		// Dein Code hier:
-		String o = "";
-		for(int i = 0; i < 6; i++) {
-		    if(i == 0 || (i == 4 && o.equals("L"))) {
-		        turnLeft();
-		    } else if(i == 1) {
-		        o = read();
-		    } else if(i == 2 || (i == 4 && o.equals("R"))) {
-		        turnRight();
-		    } else {
-		        move();
-		    }
-		} 
+		String m = "1211101";
+		for(int i = 0; i < m.length(); i++) {
+            switch(m.charAt(i)) {
+            case '0':
+                move();
+                break;
+            case '1':
+                turnLeft();
+                break;
+            case '2':
+                if(read().equals("R"))
+                    m += "11";
+                m += "0";
+                break;
+            }
+        }
     }
 
 
